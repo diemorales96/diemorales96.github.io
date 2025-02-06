c7821575ff7985247dd1e48ed3d0ebc9d4f851c7
diemorales96 <60238894+diemorales96@users.noreply.github.com>
2025-02-06 14:51:28 -0600

    Add files via upload
    Se incorporo una nueva forma de impresión de los estados visitados

--- Tarea1/cleaner.js
+++ Tarea1/cleaner.js
@@ -4,11 +4,39 @@ function reflex_agent(location, state) {
     else if (location == "B") return "LEFT";
 }
 
+let previousLocation = null;
+
+function getStateIdentifier(states) {
+    return `(${states[0]}, ${states[1]}, ${states[2]})`;
+}
+
+function getScenarioNumber(states) {
+    const scenarios = [
+        ["A", "DIRTY", "DIRTY"],
+        ["A", "DIRTY", "CLEAN"],
+        ["A", "CLEAN", "DIRTY"],
+        ["A", "CLEAN", "CLEAN"],
+        ["B", "DIRTY", "DIRTY"],
+        ["B", "DIRTY", "CLEAN"],
+        ["B", "CLEAN", "DIRTY"],
+        ["B", "CLEAN", "CLEAN"]
+    ];
+    return scenarios.findIndex(s => s[0] === states[0] && s[1] === states[1] && s[2] === states[2]) + 1;
+}
+
 function test(states) {
     var location = states[0];
     var state = location == "A" ? states[1] : states[2];
     var action_result = reflex_agent(location, state);
-    document.getElementById("log").innerHTML += `<br>Location: ${location} | Action: ${action_result}`;
+    var scenarioNumber = getScenarioNumber(states);
+    
+    // Imprimir cuando se visita un nodo diferente
+    /*if (location !== previousLocation) {
+        document.getElementById("log").innerHTML += `<br><b>Cuarto Visitado: ${location} | Escenario ${scenarioNumber}: ${getStateIdentifier(states)}</b> `;
+        previousLocation = location;
+    }*/
+    
+    document.getElementById("log").innerHTML += `<br><b>Cuarto Visitado: ${location} | Accion: ${action_result} | Escenario ${scenarioNumber}: ${getStateIdentifier(states)}</b>`;
     
     if (action_result == "CLEAN") {
         if (location == "A") states[1] = "CLEAN";
